d11e6da0331ce53fd4286d37b72d6b3ad039680d
Cris Ryan Tan <crisryantan@gmail.com>
2017-02-01 19:26:06 +0800

    display google map instead of city name only

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 
 	renderWeather ( cityData ) {
 
-		const name     = cityData.city.name;
-		const temps    = cityData.list.map( weather => weather.main.temp );
-		const pressure = cityData.list.map( weather => weather.main.pressure );
-		const humidity = cityData.list.map( weather => weather.main.humidity );
+		const name         = cityData.city.name;
+		const temps        = cityData.list.map( weather => weather.main.temp );
+		const pressure     = cityData.list.map( weather => weather.main.pressure );
+		const humidity     = cityData.list.map( weather => weather.main.humidity );
+		const { lon, lat } = cityData.city.coord;
 
 		return (
 			<tr key={ name }>
-				<td>{ name }</td>
+				<td>
+					<GoogleMap lon={ lon } lat={ lat }/>
+				</td>
 				<td>
 					<Chart
 						color={ 'orange' }
